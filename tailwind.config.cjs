d9d6358fc70b741165dfc4116db737227a391b09
ItsRauf <raufislam2003@gmail.com>
2022-10-05 12:28:15 -0400

    init: add prettier and eslint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,14 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
 };
