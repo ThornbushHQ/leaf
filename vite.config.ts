d9b7b22eace5f607344804d7f53044de51735d5b
ItsRauf <raufislam2003@gmail.com>
2022-11-01 00:09:30 -0400

    fix: add way to use env vars in html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: 'html-transform',
+    transformIndexHtml: {
+      enforce: 'pre' as const,
+      transform: (html: string) => {
+        return html.replace(/<%=(.*?)%>/g, (match, p1) => env[p1] ?? match);
+      },
+    },
+  };
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [htmlPlugin(loadEnv(mode, '.')), react()],
+}));
